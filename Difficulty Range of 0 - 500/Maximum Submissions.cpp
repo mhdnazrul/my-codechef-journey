1a4161f6918d48382a3adc31ea94c03cf2d06d5f
Nazrul Islam <mhdnazrul511.com>
2025-05-08 02:06:26 +0600

    Add solution

---
+++ Difficulty Range of 0 - 500/Maximum Submissions.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+const ll mod = 1e9 + 7;
+
+void solve() {
+  int T;
+  cin >> T;
+  int time = T * 60;
+  cout << time / 30 << endl;
+}
+
+int main() {
+  FAST_IO;
+  int T; cin >> T;while(T--)
+    solve();
+  return 0;
+}
---
+++ Difficulty Range of 0 - 500/Speciality.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IO                \
+  ios::sync_with_stdio(false); \
+  cin.tie(nullptr);
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+const ll mod = 1e9 + 7;
+
+void solve()
+{
+  int a, b, c;
+  cin >> a >> b >> c;
+  if (a > b && a > c)
+    cout << "Setter\n";
+  else if (b > a && b > c)
+    cout << "Tester\n";
+  else
+    cout << "Editorialist\n";
+}
+
+int main()
+{
+  FAST_IO;
+  int T;
+  cin >> T;
+  while (T--)
+    solve();
+  return 0;
+}
